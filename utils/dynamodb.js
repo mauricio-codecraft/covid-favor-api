5ae49159aa41e3b3899846634d2352dfe4de8c98
mauricio <mauricio@outlook.com>
2020-04-08 17:46:06 -0300

    Adding support for local development

--- utils/dynamodb.js
+++ utils/dynamodb.js
@@ -3,6 +3,5 @@ var dbClient = require('serverless-dynamodb-client');
 
 export const call = (action, params, stage) => {
   const dynamoDb = dbClient.doc;
-  console.log('dynamoDb = ', dynamoDb);
   return dynamoDb[action](params).promise();
 };
